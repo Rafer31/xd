e7523961f257a9fd9635b52134901f713186af74
Rafer31 <rafaelargotez@gmail.com>
2025-05-29 00:06:49 -0400

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatFormFieldModule} from '@angular/material/form-field'
+import { LoginComponent } from "./components/login/login.component";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatFormFieldModule],
+  imports: [RouterOutlet, MatFormFieldModule, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,19 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [MatCardModule, MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LoginComponent {
+value = 'Clear me';
+
+}
