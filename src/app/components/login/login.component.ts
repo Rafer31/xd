573b6bea5599a35ca38369dc80b944c4ae68ad9c
Rafer31 <rafaelargotez@gmail.com>
2025-05-29 11:21:46 -0400

    feat: add register mode for forms

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,19 +1,29 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [MatCardModule, MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, FormsModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, FormsModule, MatRadioModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent {
-value = 'Clear me';
+  isLoginMode = signal(true)
 
+  constructor(){
+
+  }
+  changeModeLoginForm(){
+    this.isLoginMode.set(true)
+  }
+  changeModeRegisterForm(){
+    this.isLoginMode.set(false)
+  }
 }
