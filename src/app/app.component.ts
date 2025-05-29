4a3326cde5cad6a9ef08e7aadfb7fc34e7eb69c6
Rafer31 <rafaelargotez@gmail.com>
2025-05-28 22:25:12 -0400

    xd?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MatFormFieldModule} from '@angular/material/form-field'
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatFormFieldModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
