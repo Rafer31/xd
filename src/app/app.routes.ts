17a734e928178d0a00f96245b39c4f2c544c164b
Rafer31 <rafaelargotez@gmail.com>
2025-07-02 11:47:28 -0400

    change everything :'c

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,35 +1,17 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
-import { BusesComponent } from './components/buses/buses.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { SidenavComponent } from './components/sidenav/sidenav.component';
-
+import { LoginComponent } from './shared/components/login/login.component';
+import { ToolbarComponent } from './shared/components/toolbar/toolbar.component';
 export const routes: Routes = [
     { path:'', redirectTo: '/login', pathMatch: 'full'},
     { 
         path: "login",
         component: LoginComponent
     },
-    {
-        path: "admin-dashboard",
-        component: AdminDashboardComponent,
-        children: [{
-            path: "buses",
-            component: BusesComponent
-        },
-        {
-            path: "",
-            component: AdminDashboardComponent
-        },
-        {
-            path: "sidenav",
-            component: SidenavComponent
-        }
-    ]
+    { 
+        path: "toolbar",
+        component: ToolbarComponent
     },
     {
         path: '**', redirectTo: '/login'
-    },
-    
+    },    
 ];
